0a0710a2993574c1cc6f659e88448668d1d8afa1
Waduzzaman <waduzzaman@gmail.com>
2021-10-27 02:21:00 -0400

    footer added

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
     //local MongoDB deployment ->
-    "URI": "mongodb://localhost/books"
+    "URI": "mongodb+srv://mwaduzzaman:Chapal321@cluster0.jpslj.mongodb.net/books?retryWrites=true&w=majority"
+
+
 };
